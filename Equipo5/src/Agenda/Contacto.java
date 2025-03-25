dff2285534d52daf0a4c11cf71cd6b1b3052be97
danielarog <158617300+DanielaRoG@users.noreply.github.com>
2025-03-25 12:16:36 -0600

    Equipo5

---
+++ Equipo5/src/Agenda/Contacto.java
@@ -0,0 +1,13 @@
+package Agenda;
+
+public class Contacto {
+    private String nombre;
+    private String apellido;
+    private String telefono;
+
+    public String Contacto(String nombre, String apellido,String telefono){
+        this.nombre;
+        this.apellido;
+        this.telefono;
+    }
+}
